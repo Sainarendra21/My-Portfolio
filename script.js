0e46d2a9050ffe9bfd42a52e254f111b29506114
Sai narendra <sainarendra25478@gmail.com>
2023-07-31 22:53:04 +0530

    Desktop Version

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const menu=document.querySelector(".m-links")
+    const icon=document.querySelector("m-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
